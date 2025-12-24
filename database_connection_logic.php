2f23a5963386171810bda51149ba8fbc852d9e6a
Darren-JT <darren10212005@gmail.com>
2025-12-23 23:49:41 -0500

    Database connection- review further later

---
+++ database_connection_logic.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$username = '';
+$password = '';
+$dbname = 'dolphin_crm';
+
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+?>
